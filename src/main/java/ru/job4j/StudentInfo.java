fad488f617fccae442cfc9554032fa13d8725166
yaonb <Yaonbiz@yandex.ru>
2022-12-14 01:19:30 +0300

    4.1. Разбор первой программы. #159033

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println ("Гена");
+        System.out.println ("25.10.1988");
+
+    }
+}
