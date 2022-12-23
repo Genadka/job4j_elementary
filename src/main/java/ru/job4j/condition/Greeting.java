882727949acf6350691dfbb9acb8b57b31cd65ed
yaonb <Yaonbiz@yandex.ru>
2022-12-23 20:59:57 +0300

    9.1. String #237407

---
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -0,0 +1,11 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "But I am a newbie.";
+        System.out.println(idea);
+        int year = 2022;
+        idea += year;
+        System.out.println(idea);
+    }
+}
--- src/main/java/ru/job4j/condition/SqArea.java
+++ src/main/java/ru/job4j/condition/SqArea.java
@@ -1,5 +1,4 @@
 package ru.job4j.condition;
-
 public class SqArea {
     public static double square(double p, double k) {
         double rsl = Math.pow((p / (2 * (k + 1))), 2) * k;
