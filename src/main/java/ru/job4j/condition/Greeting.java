b1b38775d0e787cde060b864f36d79c353023bca
yaonb <Yaonbiz@yandex.ru>
2022-12-24 17:12:27 +0300

    9.1. String #237407 #374104

--- src/main/java/ru/job4j/condition/Greeting.java
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -2,7 +2,9 @@ package ru.job4j.condition;
 
 public class Greeting {
     public static void main(String[] args) {
-        String idea = "But I am a newbie.";
+        String idea = "I like Java!";
+        System.out.println(idea);
+        idea += "But I am a newbie.";
         System.out.println(idea);
         int year = 2022;
         idea += year;
