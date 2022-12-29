82455dcab946837c492b34185303c5e0c45b67aa
yaonb <Yaonbiz@yandex.ru>
2022-12-29 13:27:57 +0300

    4.3.5. Множественное логическое выражение И [#9461 #375853]

--- src/test/java/ru/job4j/condition/TriangleTest.java
+++ src/test/java/ru/job4j/condition/TriangleTest.java
@@ -13,4 +13,13 @@ class TriangleTest {
         boolean result = Triangle.exist(ab, ac, bc);
         assertThat(result).isFalse();
     }
+
+    @Test
+    public void whenExist() {
+        double ab = 2.0;
+        double ac = 2.0;
+        double bc = 2.0;
+        boolean result = Triangle.exist(ab, ac, bc);
+        assertThat(result).isTrue();
+    }
 }
