22f9bfb0ea3b2d343878355d0d1ac295526e6ed8
yaonb <Yaonbiz@yandex.ru>
2023-01-15 21:53:01 +0300

    4.6.2. Switch - matcher [#334071]

---
+++ src/main/java/ru/job4j/condition/MultipleSwitchWeek.java
@@ -0,0 +1,26 @@
+package ru.job4j.condition;
+
+public class MultipleSwitchWeek {
+    public static int numberOfDay(String name) {
+        int numWeek = switch (name) {
+            case "Понедельник", "Monday" -> 1;
+            case "Вторник", "Tuesday" -> 2;
+            case "Среда", "Wednesday" -> 3;
+            case "Четверг", "Thursday" -> 4;
+            case "Пятница", "Friday" -> 5;
+            case "Суббота", "Saturday" -> 6;
+            case "Воскресенье", "Sunday" -> 7;
+            default -> 0;
+        };
+        return numWeek;
+    }
+
+    public static void main(String[] args) {
+        int nuberWeek = numberOfDay("Wednesday");
+        System.out.println(nuberWeek);
+        nuberWeek = numberOfDay("Пятница");
+        System.out.println(nuberWeek);
+        nuberWeek = numberOfDay("май");
+        System.out.println(nuberWeek);
+    }
+}
