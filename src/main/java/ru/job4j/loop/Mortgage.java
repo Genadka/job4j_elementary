d16609849c2282ba2f2af816010d2b7728244a8f
yaonb <Yaonbiz@yandex.ru>
2024-05-11 15:28:03 +0300

    5.7. Ипотека

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -4,7 +4,7 @@ public class Mortgage {
     public static int year(double amount, double salary, double percent) {
         int year = 0;
         while (amount > 0) {
-            amount += amount * percent * 0.01 - salary;
+            amount += percent * amount * 0.01 - salary;
             year++;
         }
         return year;
