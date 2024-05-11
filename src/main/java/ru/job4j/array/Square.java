e67d7d83360a9f0cca0a55da5c9a79b431455471
yaonb <Yaonbiz@yandex.ru>
2024-05-11 15:30:59 +0300

    5.7. Ипотека

--- src/main/java/ru/job4j/array/Square.java
+++ src/main/java/ru/job4j/array/Square.java
@@ -3,7 +3,7 @@ package ru.job4j.array;
 public class Square {
     public static int[] calculate(int bound) {
         int[] result = new int[bound];
-        for (int i = 0; i < result.length; i++) {
+        for (int i = 0; i < bound; i++) {
             result[i] = i * i;
         }
         return result;
@@ -11,8 +11,8 @@ public class Square {
 
     public static void main(String[] args) {
         int[] array = calculate(4);
-        for (int index = 0; index < array.length; index++) {
-            System.out.println(array[index]);
+        for (int i : array) {
+            System.out.println(i);
         }
     }
 }
