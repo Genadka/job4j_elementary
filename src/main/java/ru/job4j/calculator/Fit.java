f23bad456d2caaae9200528d45a01606a1f5a224
yaonb <Yaonbiz@yandex.ru>
2024-05-20 00:43:29 +0300

    GPT. Нейросети спешат на помощь. [#505156]

--- src/main/java/ru/job4j/calculator/Fit.java
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -2,21 +2,26 @@ package ru.job4j.calculator;
 
 public class Fit {
 
+    private static final double MAN_WEIGHT_COEFFICIENT = 1.15;
+    private static final double WOMAN_WEIGHT_COEFFICIENT = 1.15;
+
+    public static double calculateWeight(short height, double coefficient, int adjustment) {
+        return (height - adjustment) * coefficient;
+    }
+
     public static double manWeight(short height) {
-        double rsl = (height - 100) * 1.15;
-        return rsl;
+        return calculateWeight(height, MAN_WEIGHT_COEFFICIENT, 100);
     }
 
     public static double womanWeight(short height) {
-        double rsl = (height - 110) * 1.15;
-        return rsl;
+        return calculateWeight(height, WOMAN_WEIGHT_COEFFICIENT, 110);
     }
 
     public static void main(String[] args) {
         short height = 170;
-        double man = Fit.manWeight(height);
-        double woman = Fit.womanWeight(height);
-        System.out.println("Man weight with height 170 is " + man + " kilograms");
-        System.out.println("Woman weight with height 170 is " + woman + " kilograms");
+        double manWeight = Fit.manWeight(height);
+        double womanWeight = Fit.womanWeight(height);
+        System.out.printf("Man weight with height %d cm is %.2f kilograms%n", height, manWeight);
+        System.out.printf("Woman weight with height %d cm is %.2f kilograms%n", height, womanWeight);
     }
 }
